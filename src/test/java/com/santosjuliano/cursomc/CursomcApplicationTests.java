3edfdc943f403e418fd9ea77ec4200d1d17317a7
Juliano Santos <foxports@hotmail.com>
2021-09-25 16:30:58 -0300

    commit initial

---
+++ src/test/java/com/santosjuliano/cursomc/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santosjuliano.cursomc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
