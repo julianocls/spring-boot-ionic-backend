da171d00654eb85a3c5da9308d161bc10fefd4fb
Juliano Santos <foxports@hotmail.com>
2021-09-25 22:50:11 -0300

    Testando classe de dominio Categori

--- src/main/java/com/santosjuliano/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/santosjuliano/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,29 @@
 package com.santosjuliano.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.santosjuliano.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping("/categoria")
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Testando o rest...";
+	public List<Categoria> listar() {
+		
+		Categoria c1 = new Categoria(1, "Informática");
+		
+		Categoria c2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> categorias = new ArrayList<>();
+		categorias.add(c1);
+		categorias.add(c2);
+		
+		return categorias;
 	}
 }
