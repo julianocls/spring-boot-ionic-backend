e6f323d7f911778780ed4fdb31dad4435012047a
Juliano Santos <foxports@hotmail.com>
2021-09-26 12:40:40 -0300

    criando operacao de iniciacao

--- src/main/java/com/santosjuliano/cursomc/CursomcApplication.java
+++ src/main/java/com/santosjuliano/cursomc/CursomcApplication.java
@@ -1,13 +1,35 @@
 package com.santosjuliano.cursomc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.santosjuliano.cursomc.domain.Categoria;
+import com.santosjuliano.cursomc.repository.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	CategoriaRepository repo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria c1 = new Categoria(null, "Escritório");
+		Categoria c2 = new Categoria(null, "Informática");
+
+		repo.saveAll(Arrays.asList(c1, c2));
+
 	}
 
 }
