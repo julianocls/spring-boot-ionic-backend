861317345cf777c63d2174176c4dd8a47a07b94f
Juliano Santos <foxports@hotmail.com>
2021-10-03 23:33:21 -0300

    Endpoint com paginacao opcionais na requisicao

--- src/main/java/com/santosjuliano/cursomc/CursomcApplication.java
+++ src/main/java/com/santosjuliano/cursomc/CursomcApplication.java
@@ -62,6 +62,23 @@ public class CursomcApplication implements CommandLineRunner {
 
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		Categoria cat3 = new Categoria(null, "Cama, mesa e banho");
+		Categoria cat4 = new Categoria(null, "Games & Consoles");
+		Categoria cat5 = new Categoria(null, "Saúde");
+		Categoria cat6 = new Categoria(null, "Esporte & Lazer");
+		Categoria cat7 = new Categoria(null, "Audio & TV");
+		Categoria cat8 = new Categoria(null, "Vestuários");
+		Categoria cat9 = new Categoria(null, "Calçados");
+		Categoria cat10 = new Categoria(null, "Fitness");
+		Categoria cat11 = new Categoria(null, "Auto");
+		Categoria cat12 = new Categoria(null, "Perfumaria");
+		Categoria cat13 = new Categoria(null, "Eletro");
+		Categoria cat14 = new Categoria(null, "Eletrônicos");
+		Categoria cat15 = new Categoria(null, "Smartphones");
+		Categoria cat16 = new Categoria(null, "Camping");
+		Categoria cat17 = new Categoria(null, "Móveis");
+		Categoria cat18 = new Categoria(null, "Portáteis");
+		Categoria cat19 = new Categoria(null, "Importados");
 
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
@@ -71,7 +88,7 @@ public class CursomcApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7, cat8, cat9, cat10, cat11, cat12, cat13, cat14, cat15, cat16, cat17, cat18, cat19));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 
 		Estado est1 = new Estado(null, "Minas Gerais");
