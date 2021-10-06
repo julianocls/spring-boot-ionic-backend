ab63376f511c111c26284039cf1594851f2e5f7c
Juliano Santos <foxports@hotmail.com>
2021-10-05 22:32:33 -0300

    Validacao sintatica com bean.vallidation

--- src/main/java/com/santosjuliano/cursomc/service/CategoriaService.java
+++ src/main/java/com/santosjuliano/cursomc/service/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.santosjuliano.cursomc.domain.Categoria;
+import com.santosjuliano.cursomc.dto.CategoriaDTO;
 import com.santosjuliano.cursomc.repository.CategoriaRepository;
 import com.santosjuliano.cursomc.service.exception.DataIntegrityException;
 import com.santosjuliano.cursomc.service.exception.ObjectNotFoundException;
@@ -54,6 +55,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+
+	public Categoria fromDto(CategoriaDTO categoriaDTO) {
+		return new Categoria(categoriaDTO.getId(), categoriaDTO.getNome());
 	}
 
 }
