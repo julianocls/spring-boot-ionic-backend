ff9dc710081565ac9ccbcb81ff7f17df7d727923
Juliano Santos <foxports@hotmail.com>
2021-10-07 00:08:35 -0300

    personalizando erros de validacao

--- src/main/java/com/santosjuliano/cursomc/resources/ResourceExceptionHandler.java
+++ src/main/java/com/santosjuliano/cursomc/resources/ResourceExceptionHandler.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -25,4 +26,10 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validationMethodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request) {
+		ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Validation Error:", System.currentTimeMillis());
+		e.getBindingResult().getFieldErrors().forEach(f -> err.addErros(f.getField(), f.getDefaultMessage()));
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
 }
