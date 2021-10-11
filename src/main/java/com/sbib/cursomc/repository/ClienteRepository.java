456e6d19d7eabc3302b3b316f7ba83e9259b2486
Juliano Santos <foxports@hotmail.com>
2021-10-11 17:01:26 -0300

    validando emails repetidos

--- src/main/java/com/sbib/cursomc/repository/ClienteRepository.java
+++ src/main/java/com/sbib/cursomc/repository/ClienteRepository.java
@@ -2,10 +2,14 @@ package com.sbib.cursomc.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.sbib.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+
+	@Transactional(readOnly = false)
+	public Cliente findByEmail(String email);
 
 }
--- src/main/java/com/sbib/cursomc/service/validation/ClienteInsertValidator.java
+++ src/main/java/com/sbib/cursomc/service/validation/ClienteInsertValidator.java
@@ -6,12 +6,19 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.sbib.cursomc.domain.enums.TipoCliente;
 import com.sbib.cursomc.dto.ClienteNewDTO;
+import com.sbib.cursomc.repository.ClienteRepository;
 import com.sbib.cursomc.resources.FieldMessage;
 import com.sbib.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+    ClienteRepository clienteRepository;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -26,6 +33,10 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
 		if(objDto.getTipo().equals(TipoCliente.PESSOA_JURIDICA.getCodigo()) && ! BR.isValidCNPJ(objDto.getCpfOuCnpj()) ) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido!"));		
+		}
+		
+		if(clienteRepository.findByEmail(objDto.getEmail()) != null) {
+			list.add(new FieldMessage("email", "E-Mail já cadastrado!"));					
 		}
 
 		// inclua os testes aqui, inserindo erros na lista
